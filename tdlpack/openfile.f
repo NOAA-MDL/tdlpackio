cc5c2e8ee5ea7ff3a0f30a338f3c14ec508ba185
Eric Engle <eengl@users.noreply.github.com>
2019-01-03 19:58:38 -0500

    Update Fortran code
    Updated Fortran source files:
    
    - _tdlpack.pyf: F2PY signature file updated to add ier to openfile argument list.
    
    - openfile.f: Added ier to openfile argument list; Added logic to set byte order and file type values when creating new files.

--- tdlpack/openfile.f
+++ tdlpack/openfile.f
@@ -1,4 +1,4 @@
-      subroutine openfile(file,mode,lun,byteorder,ftype)
+      subroutine openfile(file,mode,lun,byteorder,ftype,ier)
       implicit none
 
       character(len=*), intent(in) :: file
@@ -6,8 +6,9 @@
       integer, intent(out) :: lun
       integer, intent(out) :: byteorder
       integer, intent(out) :: ftype
+      integer, intent(out) :: ier
 
-      integer :: ier,ios,itemp
+      integer :: ios,itemp
       character(len=:), allocatable :: cstatus
       character(len=20) :: convertx
 
@@ -15,16 +16,19 @@
       integer, save :: isysend=0
       integer, save :: lunx=65535
 
-      if(ienter.eq.0)call cksysend(6,"     ",isysend,ier)
+      ier=0
+      ios=0
 
-      lun=lunx
+      if(ienter.eq.0)call cksysend(6,"     ",isysend,ier)
+      lun=lunx+ienter
+      ienter=ienter+1
 
       if(mode.eq."r".or.mode.eq."a")then
          if(mode.eq."r")cstatus="old"
          if(mode.eq."a")cstatus="append"
 
          open(unit=lun,file=file,form="unformatted",access="stream",
-     +        status="old",iostat=ios)
+     +        status=cstatus,iostat=ios)
          read(lun,iostat=ios)itemp
          close(lun)
 
@@ -37,13 +41,16 @@
             open(unit=lun,file=file,form="unformatted",
      +           convert="big_endian",status=cstatus,iostat=ios)
          endif
-      elseif(mode.eq."w")then
-         cstatus="replace"
+      elseif(mode.eq."w".or.mode.eq."x")then
+         if(mode.eq."w")cstatus="replace"
+         if(mode.eq."x")cstatus="new"
+         byteorder=1
+         ftype=2
          open(unit=lun,file=file,form="unformatted",
      +        convert="big_endian",status=cstatus,iostat=ios)
       endif
 
-      lunx=lunx-1
+      ier=ios
 
       return
       end subroutine openfile
