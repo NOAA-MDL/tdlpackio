2581ed0b487ea52f436647cd85e05aa15d36519c
Eric Engle <eengl@users.noreply.github.com>
2019-01-06 06:56:02 -0500

    Update openfile.f
    Adding action keyword to OPEN statement.  By default, action="READWRITE" when omitted.  Therefore we explicitly set action epecially for read-only (mode="r").

--- tdlpack/openfile.f
+++ tdlpack/openfile.f
@@ -10,6 +10,7 @@
 
       integer :: ios,itemp
       character(len=:), allocatable :: caccess
+      character(len=:), allocatable :: caction
       character(len=:), allocatable :: cstatus
       character(len=20) :: convertx
 
@@ -19,6 +20,7 @@
 
       ier=0
       ios=0
+      caction="readwrite"
 
       if(ienter.eq.0)call cksysend(6,"     ",isysend,ier)
       lun=lunx+ienter
@@ -38,11 +40,15 @@
             call ckfilend(6,lun,file,isysend,byteorder,convertx,ier)
             ftype=2
             cstatus="old"
-            if(mode.eq."r")caccess="sequential"
-            if(mode.eq."a")caccess="append"
+            if(mode.eq."r")then
+               caccess="sequential"
+               caction="read"
+            elseif(mode.eq."a")then
+               caccess="append"
+            endif
             open(unit=lun,file=file,form="unformatted",
      +           convert="big_endian",status=cstatus,iostat=ios,
-     +           access=caccess)
+     +           access=caccess,action=caction)
          endif
 
       elseif(mode.eq."w".or.mode.eq."x")then
@@ -52,7 +58,8 @@
          byteorder=1
          ftype=2
          open(unit=lun,file=file,form="unformatted",
-     +        convert="big_endian",status=cstatus,iostat=ios)
+     +        convert="big_endian",status=cstatus,
+     +        action=caction,iostat=ios)
 
       endif
 
