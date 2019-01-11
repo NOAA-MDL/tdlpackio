7ad07e6cc2987f1350e8a7cbde5198ff485d8034
Eric Engle <eengl@users.noreply.github.com>
2019-01-10 22:43:31 -0500

    Update setup.py
    Added Fortran90 compiler flags.  This was used to test the new openfile.f90.  Build was successful.

--- setup.py
+++ setup.py
@@ -23,9 +23,31 @@ if __name__ == "__main__":
                          "-finit-integer=0",
                          "-finit-real=zero",
                          "-finit-logical=false"]
+            f90_flags = ["-O3",
+                         "-g",
+                         "-fbacktrace",
+                         "-fautomatic",
+                         "-finit-integer=0",
+                         "-finit-real=zero",
+                         "-finit-logical=false"]
         elif "--fcompiler=intelem" in sys.argv:
             f77_flags = ["-O3",
+                         "-g",
+                         "-traceback",
                          "-nofree",
+                         "-integer-size 32",
+                         "-real-size 32",
+                         "-auto",
+                         "-fpscomp logicals",
+                         "-fp-model strict",
+                         "-assume byterecl",
+                         "-xHost",
+                         "-align array64byte",
+                         "-fast-transcendentals",
+                         "-assume buffered_io"]
+            f90_flags = ["-O3",
+                         "-g",
+                         "-traceback",
                          "-integer-size 32",
                          "-real-size 32",
                          "-auto",
@@ -38,6 +60,7 @@ if __name__ == "__main__":
                          "-assume buffered_io"]
     else:
         f77_flags = []
+        f90_flags = []
 
     # Define Extension object. For Fortran 77 source files, use "extra_f77_compile_args".
     # For Fortran 90+ source files, use "extra_f90_compile_args".
@@ -46,7 +69,8 @@ if __name__ == "__main__":
     all_sources = ["tdlpack/_tdlpack.pyf"]+f77_sources+f90_sources
     ext = Extension(name  = '_tdlpack',
                     sources = all_sources,
-                    extra_f77_compile_args = f77_flags
+                    extra_f77_compile_args = f77_flags,
+                    extra_f90_compile_args = f90_flags
                     )
 
     # Run setup
