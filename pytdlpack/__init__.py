47a5f8febd21bf58d57b46ac86491ffbeb9504d6
Eric Engle <eengl@users.noreply.github.com>
2019-04-29 11:32:01 -0400

    Update for TdlpackRecord init
    Modified the __init__ for TdlpackRecord so that is it easier for users to create TdlpackRecord instances from stratch.
    
    The new call does not require the user to know where metadata need to be placed in the various IS* arrays.

--- pytdlpack/__init__.py
+++ pytdlpack/__init__.py
@@ -3,4 +3,5 @@ from ._pytdlpack import *
 
 from ._pytdlpack import __doc__,__pdoc__
 
-__all__ = ['TdlpackFile','TdlpackRecord','TdlpackStationRecord','TdlpackTrailerRecord','open','_read_ra_master_key']
+__all__ = ['TdlpackFile','TdlpackRecord','TdlpackStationRecord','TdlpackTrailerRecord',
+           'open','create_grid_def_dict','_read_ra_master_key']
