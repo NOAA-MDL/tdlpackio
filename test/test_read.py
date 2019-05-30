6e63defd5a115a94245147e367c33370fbfca193
Eric Engle <eengl@users.noreply.github.com>
2019-05-29 22:21:47 -0400

    Update test_read.py

--- test/test_read.py
+++ test/test_read.py
@@ -25,12 +25,13 @@ while True:
     if fin.eof: break
     n = fin.position
     if type(frec) is pytdlpack.TdlpackStationRecord:
-        frec.unpack(data=False)
+        frec.unpack()
         print "%d:%s:%d" % (n,"STATION CALL LETTER RECORD",frec.number_of_stations)
     elif type(frec) is pytdlpack.TdlpackRecord:
-        frec.unpack(data=True)
+        #frec.unpack(data=True)
+        frec.unpack(data=False)
         print "%d:d=%10d:%.9d %.9d %.9d %.10d:%3d-HR FCST:%32s:%d" % (n,frec.reference_date,
         frec.id[0],frec.id[1],frec.id[2],frec.id[3],frec.lead,frec.plain,frec.ioctet)
-        print "%s%.6f:%s%.6f" % ("MIN=",np.amin(frec.data),"MAX=",np.amax(frec.data))
-    elif type(frec) is pytdlpack.TdlpackTrailer:
+        #print "%s%.6f:%s%.6f" % ("MIN=",np.amin(frec.data),"MAX=",np.amax(frec.data))
+    elif type(frec) is pytdlpack.TdlpackTrailerRecord:
         print "%d:%s" % (n,"TRAILER RECORD")
