0480d16bee547c04e3f1b1566dfe92f5efc79695
Eric Engle <eengl@users.noreply.github.com>
2019-07-06 08:15:30 -0400

    Update __init__.py
    Removed "private" functions from __all__

--- pytdlpack/__init__.py
+++ pytdlpack/__init__.py
@@ -1,7 +1,6 @@
 # init for pytdlpack package
 from ._pytdlpack import *
-
 from ._pytdlpack import __doc__,__pdoc__
 
 __all__ = ['TdlpackFile','TdlpackRecord','TdlpackStationRecord','TdlpackTrailerRecord',
-           'open','create_grid_def_dict','_read_ra_master_key']
+           'open','create_grid_definition',]
