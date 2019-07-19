43f411fe8d03a113ec2fa02ee58770042439b00a
Eric Engle <eengl@users.noreply.github.com>
2019-07-19 19:34:23 -0400

    Update TdlpackIO.py
    Updated method for indexing.  Now using a single dictionary for a file where the value of each key is a list with N-elements where N is the total number of records in the file.  This will allow for more efficient filtering/selection in a future commit.
    
    NOTE:  Need to fix an issue with unpacking station id records...

--- TdlpackIO.py
+++ TdlpackIO.py
@@ -1,8 +1,11 @@
 import builtins
+import logging
 import numpy as np
 import pytdlpack
 import struct
 import sys
+
+import pdb
 
 ONE_MB = 1024 ** 3
 
@@ -26,9 +29,9 @@ class open(object):
             mode = mode+'b'
         elif mode == 'a':
             mode = 'wb'
-        self._file = builtins.open(filename,mode=mode,buffering=ONE_MB)
+        self._fh = builtins.open(filename,mode=mode,buffering=ONE_MB)
         self._hasindex = False
-        self._index = []
+        self._index = {}
         self.records = 0
         self.recordnumber = 0
         # Perform indexing on read
@@ -62,46 +65,63 @@ class open(object):
         """
         Perform fast indexing of records.
         """
+        self._index['offset'] = []
+        self._index['size'] = []
+        self._index['type'] = []
+        self._index['date'] = []
+        self._index['lead'] = []
+        self._index['id1'] = []
+        self._index['id2'] = []
+        self._index['id3'] = []
+        self._index['id4'] = []
         while True:
             try:
-                recdict = {}
-                pos = self._file.tell()
-                fortran_header = struct.unpack('>i',self._file.read(4))[0]
+                #recdict = {}
+                pos = self._fh.tell()
+                fortran_header = struct.unpack('>i',self._fh.read(4))[0]
                 if fortran_header >= 44:
                     bytes_to_read = 44
                 else:
                     bytes_to_read = fortran_header
-                temp = np.frombuffer(self._file.read(bytes_to_read),dtype='>i4')
+                temp = np.frombuffer(self._fh.read(bytes_to_read),dtype='>i4')
                 _header = struct.unpack('>4s',temp[2])[0].decode()
                 if _header == 'PLDT':
                     # This is a TDLPACK data record
-                    recdict['size'] = temp[1]
-                    recdict['recType'] = 'data'
-                    recdict['refDate'] = temp[6]
-                    recdict['leadTime'] = int(temp[9]-((temp[9]/1000)*1000))
-                    recdict['id1'] = temp[7]
-                    recdict['id2'] = temp[8]
-                    recdict['id3'] = temp[9]
-                    recdict['id4'] = temp[10]
+                    self._index['size'].append(temp[1])
+                    self._index['type'].append('data')
+                    self._index['date'].append(temp[6])
+                    self._index['lead'].append(int(temp[9]-((temp[9]/1000)*1000)))
+                    self._index['id1'].append(temp[7])
+                    self._index['id2'].append(temp[8])
+                    self._index['id3'].append(temp[9])
+                    self._index['id4'].append(temp[10])
                 else:
                     if temp[1] == 24 and temp[6] == 9999:
-                        recdict['recType'] = 'trailer'
-                        recdict['size'] = temp[1]
+                        self._index['size'].append(temp[1])
+                        self._index['type'].append('trailer')
+                        self._index['date'].append(None)
+                        self._index['lead'].append(None)
+                        self._index['id1'].append(None)
+                        self._index['id2'].append(None)
+                        self._index['id3'].append(None)
+                        self._index['id4'].append(None)
                     else:
                         # This is a station ID record
-                        recdict['size'] = temp[1]
-                        recdict['recType'] = 'station'
-                        recdict['id1'] = 400001000
-                        recdict['id2'] = 0  
-                        recdict['id3'] = 0
-                        recdict['id4'] = 0
+                        self._index['size'].append(temp[1])
+                        self._index['type'].append('station')
+                        self._index['date'].append(None)
+                        self._index['lead'].append(None)
+                        self._index['id1'].append(400001000)
+                        self._index['id2'].append(0)  
+                        self._index['id3'].append(0)
+                        self._index['id4'].append(0)
 
                 # At this point we have successfully identified a TDLPACK record from
                 # the file. Increment self.records and position the file pointer to
                 # now read the Fortran trailer.
                 self.records += 1 # Includes trailer records
-                self._file.seek(fortran_header-bytes_to_read,1)
-                fortran_trailer = struct.unpack('>i',self._file.read(4))[0]
+                self._fh.seek(fortran_header-bytes_to_read,1)
+                fortran_trailer = struct.unpack('>i',self._fh.read(4))[0]
 
                 # Check Fortran header and trailer for the record.
                 if fortran_header != fortran_trailer:
@@ -110,11 +130,11 @@ class open(object):
                 # NOTE: The 'offset' key contains the byte position in the file of where
                 # data record begins. A value of 12 is added to consider a 4-byte Fortran
                 # header, 4-byte "trash", and 4-byte ioctet value (already) stored on index.
-                recdict['offset'] = pos+12 # 4-byte header + 4-byte trash + 4-byte ioctet
-                self._index.append(recdict)
+                self._index['offset'].append(pos+12) # 4-byte header + 4-byte trash + 4-byte ioctet
+                #self._index.append(recdict)
 
             except(struct.error):
-                self._file.seek(0)
+                self._fh.seek(0)
                 break
 
         self._hasindex = True
@@ -123,12 +143,13 @@ class open(object):
         """
         Close the file handle
         """
-        self._file.close()
+        self._fh.close()
 
-    def read(self,num=None):
+    def read(self,num=None,unpack=True):
         """
         Read num records from the current position.
         """
+        #pdb.set_trace()
         recs = []
         if num == 0:
             return recs
@@ -138,33 +159,36 @@ class open(object):
             reclist = list(range(self.recordnumber+1,self.recordnumber+1+num))
         for n in reclist:
             nn = n-1 # Use this for the self._index referencing
+            # Test...
+            for k in self._index.keys():
+                print(n,nn,k,self._index[k][nn])
             kwargs = {}
             self.seek(n)
-            kwargs['ioctet'] = self._index[nn]['size']
-            kwargs['ipack'] = np.frombuffer(self._file.read(self._index[nn]['size']),dtype='>i4')
-            if self._index[nn]['recType'] == 'data':
-                kwargs['reference_date'] = self._index[nn]['refDate']
+            kwargs['ioctet'] = self._index['size'][nn]
+            kwargs['ipack'] = np.frombuffer(self._fh.read(self._index['size'][nn]),dtype='>i4')
+            if self._index['type'][nn] == 'data':
+                kwargs['reference_date'] = self._index['date'][nn]
                 rec = pytdlpack.TdlpackRecord(**kwargs)
-                rec.unpack()
+                if unpack: rec.unpack()
                 recs.append(rec)
-            elif self._index[nn]['recType'] == 'station':
+            elif self._index['type'][nn] == 'station':
                 kwargs['ipack'] = kwargs['ipack'].byteswap()
                 rec = pytdlpack.TdlpackStationRecord(**kwargs)
-                rec.unpack()
+                if unpack: rec.unpack()
                 recs.append(rec)
-            elif self._index[nn]['recType'] == 'trailer':
+            elif self._index['type'][nn] == 'trailer':
                 recs.append(pytdlpack.TdlpackTrailerRecord(**kwargs))
         return recs
     
-    def record(self,rec):
+    def record(self,rec,unpack=True):
         """
         Read the rec-th record.
         """
         if rec <= 0:
             return None
         else:
-            self.seek(rec)
-            return self.read(1)[0]
+            self.seek(rec-1)
+            return self.read(1,unpack=unpack)[0]
 
     def seek(self,offset):
         """
@@ -172,10 +196,10 @@ class open(object):
         """
         if self._hasindex:
             if offset == 0:
-                self._file.seek(self._index[offset]['offset'])
+                self._fh.seek(self._index['offset'][offset])
                 self.recordnumber = offset
             elif offset > 0:
-                self._file.seek(self._index[offset-1]['offset'])
+                self._fh.seek(self._index['offset'][offset])
                 self.recordnumber = offset
     
     def tell(self):
