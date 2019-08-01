658a72e98348de5ccf8daafd29992289359ce73c
Eric Engle <eengl@users.noreply.github.com>
2019-08-01 12:03:59 -0400

    Create test_tdlpackio_select.py

---
+++ test/test_tdlpackio_select.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pdb
+import os
+import sys
+
+import setuptools
+platform = setuptools.distutils.util.get_platform()
+build_path = './build/lib.'+platform+'-'+str(sys.version_info.major)+'.'+str(sys.version_info.minor)
+sys.path.insert(0,build_path)
+import pytdlpack
+import TdlpackIO
+
+# ---------------------------------------------------------------------------------------- 
+# Point stdout to null
+# ---------------------------------------------------------------------------------------- 
+fout = open(os.devnull,'w')
+sys.stdout = fout
+
+# ---------------------------------------------------------------------------------------- 
+# Open vector TDLPACK file; iterate; close
+# ---------------------------------------------------------------------------------------- 
+f = TdlpackIO.open("../../sampledata/hre201701")
+rec = f.select(date=2017011512)
+f.close()
+
+# ---------------------------------------------------------------------------------------- 
+# Open gridded TDLPACK file; iterate; close
+# ---------------------------------------------------------------------------------------- 
+f = TdlpackIO.open("../../sampledata/gfspkd47.2017020100.sq")
+f.seek(0)
+for rec in f:
+    print(f.recordnumber,type(rec),rec.id)
+f.close()
+
+fout.close()
