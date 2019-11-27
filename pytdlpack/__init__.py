f37652b609a2c214cc3e0be2d5e542813632ee25
Eric Engle <eengl@users.noreply.github.com>
2019-11-26 22:17:08 -0500

    Update __init__.py
    Adding import of _grid_definitions such that the grids dict is visible from top-level init.

--- pytdlpack/__init__.py
+++ pytdlpack/__init__.py
@@ -2,7 +2,9 @@
 from ._pytdlpack import *
 from ._pytdlpack import __doc__,__pdoc__
 
+from ._grid_definitions import grids
+
 from .version import version as __version__
 
 __all__ = ['__version__','TdlpackFile','TdlpackRecord','TdlpackStationRecord','TdlpackTrailerRecord',
-           'open','create_grid_definition',]
+           'open','create_grid_definition','grids']
