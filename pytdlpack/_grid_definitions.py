49636f2bef0b42523c206d1af1c2f7a451e98427
Eric Engle <eengl@users.noreply.github.com>
2019-12-03 07:51:38 -0500

    Update _grid_definitions.py
    Added various GFS- and NAM-based MOS archive grid definitions.
    
    Renamed blend_* grids names to nbm* -- to shorten the names.

--- pytdlpack/_grid_definitions.py
+++ pytdlpack/_grid_definitions.py
@@ -57,9 +57,69 @@ _blend_puertorico = { 'proj4_alias': 'merc',
                       'meshlength': 1250.000000
                      }
 
-grids = { 'blend_alaska': _blend_alaska,
-          'blend_conus': _blend_conus,
-          'blend_hawaii': _blend_hawaii,
-          'blend_oceanic': _blend_oceanic,
-          'blend_puertorico': _blend_puertorico
+_mos_nps_23km = { 'proj4_alias': 'stere',
+                  'proj': 5,
+                  'nx': 593,
+                  'ny': 337,
+                  'latll': 2.8320,
+                  'lonll': 150.0003,
+                  'stdlat': 60.0000,
+                  'orientlon': 105.0000,
+                  'meshlength': 23812.500000
+                     }
+
+_mos_nps_47km = { 'proj4_alias': 'stere',
+                  'proj': 5,
+                  'nx': 297,
+                  'ny': 169,
+                  'latll': 2.8320,
+                  'lonll': 150.0003,
+                  'stdlat': 60.0000,
+                  'orientlon': 105.0000,
+                  'meshlength': 47625.000000
+                     }
+
+_mos_nps_95km = { 'proj4_alias': 'stere',
+                  'proj': 5,
+                  'nx': 149,
+                  'ny': 85,
+                  'latll': 2.8320,
+                  'lonll': 150.0003,
+                  'stdlat': 60.0000,
+                  'orientlon': 105.0000,
+                  'meshlength': 95250.000000
+                     }
+
+_nammos_ncep151 = { 'proj4_alias': 'stere',
+                    'proj': 5,
+                    'nx': 425,
+                    'ny': 281,
+                    'latll': 0.7279,
+                    'lonll': 150.3583,
+                    'stdlat': 60.0000,
+                    'orientlon': 110.0000,
+                    'meshlength': 33812.000000
+                  }
+
+_nammos_ncep221_exp = { 'proj4_alias': 'lcc',
+                        'proj': 3,
+                        'nx': 349,
+                        'ny': 198,
+                        'latll': 9.5803,
+                        'lonll': 150.7806,
+                        'stdlat': 50.0000,
+                        'orientlon': 107.0000,
+                        'meshlength': 32463.410156
+                      }
+
+grids = { 'nbmak': _blend_alaska,
+          'nbmco': _blend_conus,
+          'nbmhi': _blend_hawaii,
+          'nbmoc': _blend_oceanic,
+          'nbmpr': _blend_puertorico,
+          'gfs23': _mos_nps_23km,
+          'gfs47': _mos_nps_47km,
+          'gfs95': _mos_nps_95km,
+          'nam151': _nammos_ncep151,
+          'nam221': _nammos_ncep221_exp
         }
