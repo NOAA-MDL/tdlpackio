597265d2a94ca28248da166af3d5fed1b7d3ad81
Eric Engle <eengl@users.noreply.github.com>
2020-01-23 12:15:58 -0500

    Update docs

--- create_docs.sh
+++ create_docs.sh
@@ -3,4 +3,4 @@
 BUILD_DIR=$(find $PWD/build -mindepth 1 -name "lib.*" -type d)
 export PYTHONPATH=$BUILD_DIR:$PYTHONPATH
 pdoc --html --html-no-source --overwrite --html-dir 'docs' pytdlpack
-#pdoc --html --html-no-source --overwrite --html-dir 'docs' TdlpackIO
+pdoc --html --html-no-source --overwrite --html-dir 'docs' TdlpackIO
