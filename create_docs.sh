714269c6604cf0f1b025b9153bc231fc09586c28
Eric Engle <eengl@users.noreply.github.com>
2020-01-27 10:55:36 -0500

    Update docs
    Updated create_docs.sh to rebuild (using GNU) so any documentation changes from Python sources make it into the documentation HTML.

--- create_docs.sh
+++ create_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+python setup.py build_ext --fcompiler=gnu95 build
+
 BUILD_DIR=$(find $PWD/build -mindepth 1 -name "lib.*" -type d)
 export PYTHONPATH=$BUILD_DIR:$PYTHONPATH
 pdoc --html --html-no-source --overwrite --html-dir 'docs' pytdlpack
