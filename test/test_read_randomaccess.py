c8eebd7ffc6d8b992db6849ffcdc9e9647cb1d35
Eric Engle <eengl@users.noreply.github.com>
2020-02-12 10:56:40 -0500

    Create test_read_randomaccess.py
    Test script to read a gridded random access file.

---
+++ test/test_read_randomaccess.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# ---------------------------------------------------------------------------------------- 
+# Import Modules
+# ---------------------------------------------------------------------------------------- 
+import numpy as np
+import setuptools
+import sys
+
+platform = setuptools.distutils.util.get_platform()
+build_path = './build/lib.'+platform+'-'+str(sys.version_info.major)+'.'+str(sys.version_info.minor)
+sys.path.insert(0,build_path)
+import pytdlpack
+
+f = pytdlpack.open('blend.analysisgrconst.co.ra')
+recs = f.read(all=True)
+for r in recs:
+   r.unpack(data=True)
+
+f.close()
