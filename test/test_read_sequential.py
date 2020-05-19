f602c2d98d20ce1db75be09f5d572cb84250cde8
Eric Engle <eengl@users.noreply.github.com>
2020-05-19 13:10:23 -0400

    Create test_read_sequential.py
    New test script.

---
+++ test/test_read_sequential.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# ---------------------------------------------------------------------------------------- 
+# Import Modules
+# ---------------------------------------------------------------------------------------- 
+import copy
+import filecmp
+import numpy as np
+import setuptools
+import sys
+
+platform = setuptools.distutils.util.get_platform()
+build_path = './build/lib.'+platform+'-'+str(sys.version_info.major)+'.'+str(sys.version_info.minor)
+sys.path.insert(0,build_path)
+import pytdlpack
+
+file_input = "sampledata/gfspkd47.2017020100.sq"
+file_output = "test.sq"
+
+f = pytdlpack.open(file_input)
+fout = pytdlpack.open(file_output,mode="w")
+recs = f.read(all=True)
+for r in recs:
+   r.unpack()
+   newrec = copy.deepcopy(r)
+   newrec.pack()
+   fout.write(newrec)
+
+f.close()
+fout.close()
+
+filecompare = filecmp.cmp(file_input,file_output)
+print("filecmp result:",filecompare)
