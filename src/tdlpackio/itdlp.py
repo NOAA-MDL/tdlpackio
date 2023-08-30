f35b5c45f385bc4f7847be2fde83ebfdb1873318
Eric Engle <eengl@users.noreply.github.com>
2023-08-30 14:41:12 -0400

    Update for itdlp.py
    Added -rec, -q, -tdlp options.

--- src/tdlpackio/itdlp.py
+++ src/tdlpackio/itdlp.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import signal
 import sys
 import tdlpackio
 
@@ -7,10 +8,43 @@ DESCRIPTION = """
 itdlp - a command line utility to inventory MDL's MOS-2000 TDLPACK files.
 """
 
-V_HELP = """
-Verbose inventory.  Data are unpacked.  Print statistics (max, min, mean values) and
-information and counts of missing values.
+QUIET_HELP = """
+Quiet mode.  Suppress all inventory output.  Only errors are printed to
+standard output.
 """
+
+REC_HELP = """
+Inventory TDLPACK records with record number REC.  REC can be a comma-delimited
+list of numbers and/or ranges (e.g. 1,2,5,6,10-20)
+"""
+
+TDLP_HELP = """
+Output TDLPACK records to new sequential file FILE.
+"""
+
+VERBOSE_HELP = """
+Verbose inventory.  Data are unpacked and statistics (max, min, mean values); 
+counts of missing values are provided.
+"""
+
+def process_user_rec(rec):
+    """
+    Process record numbers and ranges from the user via -rec argument.
+    """
+    recs = list()
+    for r in rec.split(','):
+        if '-' in r:
+            start, stop = r.split('-')
+            recs += list(range(int(start),int(stop)+1,1))
+        else:
+            recs.append(int(r))
+    return recs
+
+
+def signal_handler(sig, frame):
+    """
+    """
+    exit(0)
 
 
 def verbose(rec):
@@ -42,20 +76,48 @@ def verbose(rec):
 def _itdlp_main():
     """
     """
+    # Define and parse args
     parser = argparse.ArgumentParser(prog='itdlp',
                                      description=DESCRIPTION)
     parser.add_argument('file')
-    parser.add_argument('-v', dest='verbose', action='store_true', help=V_HELP)
+    parser.add_argument('-q', dest='quiet', action='store_true', help=QUIET_HELP)
+    parser.add_argument('-rec', dest='rec', type=process_user_rec, help=REC_HELP)
+    parser.add_argument('-tdlp', dest='tdlp', action='store', help=TDLP_HELP)
+    parser.add_argument('-v', dest='verbose', action='store_true', help=VERBOSE_HELP)
     args = parser.parse_args()
 
+    # Capture signals
+    signal.signal(signal.SIGINT,signal_handler)
+
+    # Open input file and create iterable collection of records based on user
+    # supplied args
     f = tdlpackio.open(args.file)
-    for rec in f:
+    if 'rec' in args:
+        fiter = [f[r] for r in args.rec]
+    else:
+        fiter = f
 
-        recstr = list(rec.__str__().split(':'))
-        print(':'.join([s for s in recstr]),flush=True)
+    # Check if writing to new file.
+    if args.tdlp is not None: fout = tdlpackio.open(args.tdlp,mode='w',format='sequential')
 
-        if not hasattr(rec,'type'): continue
+    # Iterate over records
+    for rec in fiter:
 
-        if args.verbose: print(verbose(rec),flush=True)
+        # Default inventory print
+        if not args.quiet:
+            recstr = list(rec.__str__().split(':'))
+            print(':'.join([s.rstrip() for s in recstr]),flush=True)
 
+        #
+        if not hasattr(rec,'_data'): continue
+
+        # Verbose mode
+        if args.verbose and not args.quiet: print(verbose(rec),flush=True)
+
+        # Output records to new file
+        if args.tdlp is not None: fout.write(rec)
+
+    # Close input file
     f.close()
+    # Close output file
+    if args.tdlp is not None: fout.close()
