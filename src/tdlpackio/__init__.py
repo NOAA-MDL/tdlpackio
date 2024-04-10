33827e22d986968811608d970b76f6e4b02a80fa
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2024-04-10 12:29:50 -0400

    Update __init__.py
    Added TdlpackID to __all__.

--- src/tdlpackio/__init__.py
+++ src/tdlpackio/__init__.py
@@ -4,4 +4,5 @@ from ._tdlpackio import __doc__
 
 from .version import version as __version__
 
-__all__ = ['__version__','open','TdlpackRecord','TdlpackStationRecord','TdlpackTrailerRecord',]
+__all__ = ['__version__', 'open', 'TdlpackRecord', 'TdlpackStationRecord',
+           'TdlpackTrailerRecord', 'TdlpackID']
