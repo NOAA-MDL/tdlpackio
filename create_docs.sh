0e6445c0ac81b500fdbea95b133d6ea84c23b99c
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2025-04-18 11:20:09 -0400

    Version bump to 1.1.3 and docs update.

--- create_docs.sh
+++ create_docs.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-set -x
 
-python3 setup.py build_ext --fcompiler=gnu95 build
-
-unset PYTHONPATH
 BUILD_DIR=$(find $PWD/build -mindepth 1 -name "lib.*" -type d)
 export PYTHONPATH=$BUILD_DIR:$PYTHONPATH
-pdoc --html -c show_source_code=True --force --output-dir 'docs' pytdlpack
-pdoc --html -c show_source_code=True --force --output-dir 'docs' TdlpackIO
+
+VER=$(python -c "import pytdlpack; print(f\"pytdlpack v{pytdlpack.__version__}\")")
+
+pdoc --footer-text "$VER" -o 'docs' pytdlpack
+pdoc --footer-text "$VER" -o 'docs' TdlpackIO
+
+touch index.html
+echo "<h1>$VER</h1>" >> index.html
+echo "<li> <a href=pytdlpack.html>pytdlpack docs</a> </li>" >> index.html
+echo "<li> <a href=TdlpackIO.html>TdlpackIO docs</a> </li>" >> index.html
+mv -v index.html docs/.
